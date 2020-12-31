af723ad78ba78d1ef7244b93933ed297c910ff5c
Kingsley Zhong <40475664+Generaleoley@users.noreply.github.com>
2020-12-30 21:46:44 -0700

    Update bstools.py
    i'll finish in a sec... downloading file so I don't edit in web

--- bstools/bstools.py
+++ bstools/bstools.py
@@ -1,3 +1,21 @@
 import discord
 from redbot.core import Config, checks, commands
 
+# I'm not doing a seperate tags class it's not needed because there isn't SQL 
+class BSTools(commands.Cog):
+  def __init__(self,  bot):
+    self.bot = bot;
+    self.config = Config.get_conf(self, identifier=9034283423)
+    default_user = { # todo feel free to do multiple tags, I really don't have the time rn @TPK
+      'tag': None
+    }
+    self.config.register_user(**default_user) # User becauser it doesn't matter what server they are in
+    
+   def getTag(self, user: int):
+    """Returns tag (str) without # or None if it wasn't saved"""
+    self.config.user(int).tag()
+    
+   def saveTag(self, user: int, tag: str):
+    """Saves user tag. Doesn't return anything. Throws ValueError if the tag is invalid"""
+    
+    
